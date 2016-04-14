766f5570b8980618ce7564f2ad008595f54dafd3
OkaAryanata <OkaAryanata@192.168.160.1>
2016-04-14 17:48:33 +0700

    Merge origin/master
    Conflicts:
    	test/src/test/Anggota.java
    	test/src/test/AplikasiPerpus.java

--- test/src/test/AplikasiPerpus.java
+++ test/src/test/AplikasiPerpus.java
@@ -36,9 +36,127 @@ public class AplikasiPerpus {
         this.petugas.addPetugas(p1);
         this.petugas.addPetugas(p2);
         this.petugas.addPetugas(p3);
+        System.out.println(barang.getBooklist(0).toString());
+        System.out.println("====================");
+        System.out.println(petugas.getDaftarPetugas(2).toString());
+        System.out.println("====================");
+        //System.out.println(anggota.getDaftarAnggota(2).toString());
+        
+        System.out.println("hello");
+        //System.out.println(anggota.toString());
     }
     
     public void menuAwal(){
+        Scanner sc = new Scanner(System.in);
+        this.s = sc;
+        int log=0;
+        do{
+            try{
+                System.out.println();
+                System.out.println("Menu");
+                System.out.println("1. Daftar");
+                System.out.println("2. Login");
+                System.out.println("3. Exit");
+                System.out.print("Input Menu : ");
+                log = s.nextInt();
+                switch(log){
+                    case 1:
+                    this.menuDaftar();
+                    case 2:
+                    //this.menuLogin();
+                        System.out.println("");
+                    case 3:
+                    System.out.println("Terima kasih");
+                    System.exit(0);
+                    default:
+                    System.out.println("Input menu antara 1-3");
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Inputan harus angka");
+            }finally{
+                Scanner scan = new Scanner(System.in);
+                this.s = scan;
+            }
+        }while(log!=3);
+    }
+    
+    public void menuDaftar(){
+        Scanner sc = new Scanner(System.in);
+        this.s = sc;
+        System.out.println();
+        System.out.println("Daftar");
+        System.out.println("1. Anggota");
+        System.out.println("2. Petugas");
+        System.out.println("3. Back");
+        System.out.print("Input Menu : ");
+        int log = s.nextInt();
+        switch(log){
+            case 1:
+            this.daftarAnggota();
+            case 2:
+            this.daftarPetugas();
+            case 3:
+            this.menuAwal();
+            default:
+            System.out.println("Input menu antara 1-3");
+        }
+    }
+    
+    public void daftarAnggota(){
+        Scanner sc = new Scanner(System.in);
+        this.s = sc;
+        System.out.println();
+        System.out.println("Daftar Anggota");
+        System.out.print("Masukkan Username : ");
+        String username = s.next();
+        System.out.print("Masukkan Password : ");
+        String password = s.next();
+        System.out.print("Nama Anggota   : ");
+        String nama = s.next();
+        System.out.print("NIM  : ");
+        String nim = s.next();
+        System.out.print("Jenis Kelamin  : ");
+        String kelamin = s.next();
+        System.out.print("Alamat    : ");
+        String alamat = s.next();
+        System.out.print("Fakultas    : ");
+        String fakultas = s.next();
+        System.out.print("Jurusan    : ");
+        String jurusan = s.next();
+        
+        Anggota a = new Anggota(username, password, nim, nama, kelamin, alamat, fakultas, jurusan);
+        this.anggota.addAnggota(a);
+        System.out.println("Sukses");
+        //this.menuLogin();
+        System.out.println("====================");
+        System.out.println(anggota.getDaftarAnggota(0).toString());
         
     }
+    
+    public void daftarPetugas(){
+        Scanner sc = new Scanner(System.in);
+        this.s = sc;
+        System.out.println();
+        System.out.println("Daftar Petugas");
+        System.out.print("Masukkan Username : ");
+        String username = s.next();
+        System.out.print("Masukkan Password : ");
+        String password = s.next();
+        System.out.print("Nama Petugas   : ");
+        String nama = s.next();
+        System.out.print("Jenis Kelamin  : ");
+        String kelamin = s.next();
+        System.out.print("Alamat    : ");
+        String alamat = s.next();
+        System.out.print("Nip    : ");
+        String nip = s.next();
+        
+        Petugas p = new Petugas(username, password, nip, nama, alamat, kelamin);
+        this.petugas.addPetugas(p);
+        System.out.println("Sukses");
+        //this.menuLogin();
+        System.out.println(petugas.getDaftarPetugas(3).toString());
+    }
+    
+    
 }
